4d925264f00dddc49a6037333cadd00893b383c0
Pranav Sangani <psangani@purdue.edu>
2025-01-20 19:41:16 -0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Import your App component
-import './index.css'; // Import global styles if any
+import App from './App'; // Your main component
+import './index.css'; // Global styles if any
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
