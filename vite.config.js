efae2987b0a59c00b7e97e20539c43c7f565ffdb
Pranav Sangani <psangani@purdue.edu>
2025-01-20 20:12:02 -0500

    linking index.html

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './sorting-algo-visualizer/', // Replace with your repository name
+  base: '/sorting-algo-visualizer/', // Your GitHub repository name
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'src/index.html', // Use the moved index.html as the entry point
+    },
+  },
 });
