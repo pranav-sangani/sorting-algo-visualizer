7466a2bec5bc0e6d04fc290a9087e109d3351a8b
Pranav Sangani <psangani@purdue.edu>
2025-01-20 19:51:53 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/sorting-algo-visualizer/', // Replace with your repository name
+  base: './sorting-algo-visualizer/', // Replace with your repository name
   plugins: [react()],
 });
